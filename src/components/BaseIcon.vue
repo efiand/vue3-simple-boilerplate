b35d96fdb71f33d7d50981d083efa1b636d42974
Andrey Efimov <efiand@ya.ru>
2024-03-10 18:04:48 +0300

    Временное решение для иконок

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,20 @@
+<template>
+	<svg focusable="false">
+		<use :href="`/images/icons/${id}.svg`" />
+	</svg>
+</template>
+
+<script setup>
+defineProps({
+	id: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+svg {
+	fill: currentColor;
+}
+</style>
