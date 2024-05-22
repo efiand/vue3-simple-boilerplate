01a63144e02260027c4da0d1993d6df9870b69d5
Andrey Efimov <efiand@ya.ru>
2024-05-22 17:38:37 +0300

    Add postcss-url

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,18 @@
 const autoprefixer = require('autoprefixer');
 const mqpacker = require('postcss-sort-media-queries');
+const url = require('postcss-url');
 
 const config = {
 	syntax: 'postcss-scss',
 	plugins: [
 		autoprefixer,
 		mqpacker,
+		url([
+			{
+				filter: '**/icons/*',
+				url: 'inline',
+			},
+		]),
 	],
 };
 
